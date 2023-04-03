acb62de31f8d42025b5e617b0be5f9c314baa77f
GonzaloHaag <111447167+GonzaloHaag@users.noreply.github.com>
2023-04-03 19:19:32 -0300

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//Abrir menu mobile
+
+const openMenu = document.querySelector("#open-menu-mobile");
+
+const menuMobile = document.querySelector(".menu-mobile");
+
+const closeMenu = document.querySelector("#close-menu");
+
+openMenu.addEventListener("click",()=>{
+
+
+    menuMobile.classList.add("active");
+
+})
+closeMenu.addEventListener("click",()=>{
+    menuMobile.classList.remove("active");
+});
+
+//Funcionalidad para cerrar menu mobile si clickeo un link
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link)=>{
+    link.addEventListener("click",()=>{
+        menuMobile.classList.remove("active");
+    })
+})
